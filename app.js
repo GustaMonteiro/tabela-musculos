e825f70635dbf5a6073381ac09b8ecc6aa4461e3
GustaMonteiro <gcvm6070@gmail.com>
2023-02-25 21:44:16 -0300

    Refactor html, js and add more muscles

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cardList = document.querySelector('#card-list');
 
 const allCards = [];
 
-const musclesJson = JSON.parse('[{"name": "SUBESCAPULAR", "origin": "Fossa subescapular", "insertion": "Tubérculo menor do úmero (manguito rotador)", "inervation": "Nervo subescapular", "action": "Rotação medial e adução do ombro", "movePlan": "Transverso", "imageId": 0}, {"name": "SUPRA-ESPINHAL", "origin": "Fossa supra espinhal da escápula", "insertion": "Tubérculo maior do úmero (manguito rotador)", "inervation": "Nervo supraescapular", "action": "Abdução do ombro", "movePlan": "Frontal", "imageId": 1}]');
+const musclesJson = JSON.parse('[{"name": "SUBESCAPULAR", "origin": "Fossa subescapular", "insertion": "Tubérculo menor do úmero (manguito rotador)", "inervation": "Nervo subescapular", "action": "Rotação medial e adução do ombro", "movePlan": "Transverso", "imageId": 0}, {"name": "SUPRA-ESPINHAL", "origin": "Fossa supra espinhal da escápula", "insertion": "Tubérculo maior do úmero (manguito rotador)", "inervation": "Nervo supraescapular", "action": "Abdução do ombro", "movePlan": "Frontal", "imageId": 1}, {"name": "INFRA-ESPINHAL", "origin": "Superfície posterior da escápula", "insertion": "Tubérculo maior do úmero (manguito rotador)", "inervation": "Nervo supraescapular", "action": "Rotação lateral e adução do ombro", "movePlan": "Transverso", "imageId": 2}, {"name": "REDONDO MENOR", "origin": "Escápula posterior, borda lateral", "insertion": "Tubérculo maior do úmero (manguito rotador)", "inervation": "Nervo axilar", "action": "Rotação lateral e adução do ombro", "movePlan": "Transverso", "imageId": 3}, {"name": "REDONDO MAIOR", "origin": "Face posterior do ângulo inferior da escápula", "insertion": "Lábio medial do Sulco intertubercu lar do úmero", "inervation": "Nervo subescapular", "action": "Rotação medial e adução do ombro", "movePlan": "Sagital, frontal e transverso", "imageId": 4}]');
 
 class Muscle {
   constructor(name, origin, insertion, inervation, action, movePlan, imageId) {
@@ -81,21 +81,6 @@ for(let muscle of musclesJson) {
   allCards.push(createCard(new Muscle(muscle.name, muscle.origin, muscle.insertion, muscle.inervation, muscle.action, muscle.movePlan, muscle.imageId)));
 }
 
-// for(let card of allCards){
-//   cardList.appendChild(card);
-// }
-
-// const subescapular = new Muscle("SUBESCAPULAR", "Fossa subescapular", "Tubérculo menor do úmero (manguito rotador)", "Nervo subescapular", "Rotação medial e adução do ombro", "Transverso", "0");
-// const supraEspinhal = new Muscle("SUPRA-ESPINHAL", "Fossa supra espinhal da escápula", "Tubérculo maior do úmero (manguito rotador)", "Nervo supraescapular", "Abdução do ombro", "Frontal", "1");
-
-// console.log(JSON.stringify(subescapular));
-
-// for (let nome of nomes) {
-//   const newLi = document.createElement('li');
-//   newLi.innerText = nome;
-//   allItems.push(newLi);
-// }
-
 const showAllCards = () => {
   for (let card of allCards) {
     cardList.appendChild(card);
@@ -123,24 +108,4 @@ searchBar.addEventListener('input', e => {
   }
 })
 
-// cardList.appendChild(createCard(subescapular));
-// cardList.appendChild(createCard(supraEspinhal));
-
 showAllCards();
-
-      // <div class="col">
-      //   <div class="card h-100">
-      //     <img src="images/2.jpg" class="card-img-top" alt="...">
-      //     <div class="card-body">
-      //       <h5 class="card-title">Card title</h5>
-      //       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      //     </div>
-      //     <ul class="list-group list-group-flush">
-      //       <li class="list-group-item">An item</li>
-      //       <li class="list-group-item">A second item lorem</li>
-      //       <li class="list-group-item">A third item</li>
-      //       <li class="list-group-item">A third item</li>
-      //       <li class="list-group-item">A third item</li>
-      //     </ul>
-      //   </div>
-      // </div>
